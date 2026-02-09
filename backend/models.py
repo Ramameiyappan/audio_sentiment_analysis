21e991a2e63b8ba474484d620737ea20ee517f70
Ramameiyappan <meiyappanvdm@gmail.com>
2026-02-09 23:00:40 +0530

    fix

--- backend/models.py
+++ backend/models.py
@@ -1,20 +1,20 @@
 import whisper
+import torch
 from transformers import (
-    Wav2Vec2FeatureExtractor,
-    Wav2Vec2ForSequenceClassification,
+    Wav2Vec2FeatureExtractor, 
+    Wav2Vec2ForSequenceClassification, 
     pipeline
 )
 
 def load_models():
     AUDIO_MODEL = "superb/wav2vec2-base-superb-er"
-
+    
     feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(AUDIO_MODEL)
-
     audio_model = Wav2Vec2ForSequenceClassification.from_pretrained(AUDIO_MODEL)
-    audio_model.to("cpu") 
+    audio_model.to("cpu")
     audio_model.eval()
 
-    whisper_model = whisper.load_model("base", device="cpu")
+    whisper_model = whisper.load_model("tiny", device="cpu")
 
     text_sentiment_model = pipeline(
         "sentiment-analysis",
